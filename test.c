20df7d03c2ecbcf2f1788b035258b3d1e3489115
zhudachang123 <107539613+zhudachang123@users.noreply.github.com>
2022-06-17 23:02:16 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//#include<stdio.h>
+//int main()
+//{ 
+//	printf("dd");
+//	return 0;
+//}
+
+
+//
+//
+//#include<stdio.h>
+//int main()
+//{
+//		printf("%d\n", 100);//ÕûÐÎd
+//		return 0;
+//	}
+
+//#include<stdio.h>
+//int main()
+//{
+//   printf("%d\n",sizeof (char));
+//   printf("%d\n", sizeof(short));
+//   printf("%d\n", sizeof(int));
+//   printf("%d\n", sizeof(long long));
+//   printf("%d\n", sizeof(float));
+//   printf("%d\n", sizeof(double));
+//   return 0; 
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int age = 18;
+//    double weight = 166.8;
+//	
+//	age = age+1;
+//	weight = weight - 20;
+//	printf("%d\n", age);
+//	printf("%lf\n", weight);
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", & a, & b);
+	sum = a + b;
+	printf("sum=%d\n", sum);
+	return 0;
+}
+
+
+
+ 
